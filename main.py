fcd2ce7cbee1fea091109076cd9a781502e7eb30
Christopher Vestman <christophervestman@gmail.com>
2024-11-14 17:54:33 +0100

    fix: updates db if it exists

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ if __name__ == "__main__":
             create_db()
             convert_lua_to_json("MPlusTracker.lua", "MPlusTracker.json")
             load_data_to_db("MPlusTracker.json")
+        else:
+            print("Updating DB")
+            # Assuming new data
+            convert_lua_to_json("MPlusTracker.lua", "MPlusTracker.json")
+            load_data_to_db("MPlusTracker.json")
 
     except Exception as e:
         print(f"Error from main.py: {e}")
