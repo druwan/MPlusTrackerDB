0e1a1371044558c3ed3016c04edcd4a787ba1761
Christopher Vestman <christophervestman@gmail.com>
2024-12-03 22:27:45 +0100

    fix: update psycopg

--- db.py
+++ db.py
@@ -2,21 +2,11 @@ import json
 import os
 
 import polars as pl
-import psycopg2
 from dotenv import load_dotenv
-from psycopg2 import sql
-from psycopg2.extras import execute_values
+from psycopg import connect,sql
+from psycopg.errors import UniqueViolation
 
 load_dotenv()
-
-conn = psycopg2.connect(
-    dbname="postgres",
-    user=os.getenv("DBUSER"),
-    password=os.getenv("DBPASSWD"),
-    host=os.getenv("DBHOST"),
-    port=os.getenv("DBPORT"),
-)
-
 
 def db_exists():
     """
@@ -24,30 +14,22 @@ def db_exists():
     Returns True if it exists, False otherwise.
     """
     try:
-        conn = psycopg2.connect(
+         with connect(
             dbname="postgres",
             user=os.getenv("DBUSER"),
             password=os.getenv("DBPASSWD"),
             host=os.getenv("DBHOST"),
             port=os.getenv("DBPORT"),
-        )
-        conn.autocommit = True
-        with conn.cursor() as cursor:
-            cursor.execute(
-                "SELECT 1 FROM pg_database WHERE datname = %s;",
-                (os.getenv("DBNAME"),),
-            )
-            if cursor.fetchone():
-                return True
-            else:
-                print(f"DB {os.getenv("DBNAME")} does not exist, creating...")
-                return False
-
-    except psycopg2.Error as e:
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT 1 FROM pg_database WHERE datname = %s;",
+                    (os.getenv("DBNAME"),),
+                )
+                return cursor.fetchone() is not None
+    except Exception as e:
         print(f"Error checking db: {e}")
-    finally:
-        if conn:
-            conn.close()
+        return False
 
 
 def create_db():
@@ -56,25 +38,45 @@ def create_db():
     """
     new_db = os.getenv("DBNAME")
     try:
-        conn = psycopg2.connect(
+        with connect(
             dbname="postgres",
             user=os.getenv("DBUSER"),
             password=os.getenv("DBPASSWD"),
             host=os.getenv("DBHOST"),
             port=os.getenv("DBPORT"),
-        )
-        conn.autocommit = True
-        with conn.cursor() as cursor:
-            cursor.execute(
-                sql.SQL("CREATE DATABASE {};").format(sql.Identifier(new_db))
-            )
-            print("db created")
-
-    except psycopg2.Error as e:
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    sql.SQL("CREATE DATABASE {};").format(sql.Identifier(new_db))
+                )
+                print("db created")
+    except Exception as e:
         print(f"Error creating db: {e}")
-    finally:
-        if conn:
-            conn.close()
+
+
+def ensure_unique_constraint(conn):
+    """
+    Ensure the unique constraint on the 'runs' table.
+    """
+    with conn.cursor as cursor:
+        cursor.execute(
+            """
+                SELECT conname
+                FROM pg_constraint
+                WHERE conrelid = 'runs'::regclass 
+                    AND contype = 'u' 
+                    AND conname = 'unique_start_time';
+            """
+        )
+        if cursor.fetchone() is None:
+            # Add constraint
+            cursor.execute(
+                """
+                    ALTER TABLE runs
+                    ADD CONSTRAINT unique_start_time UNIQUE (start_time);
+                """
+            )
+            print("Unique constraint added to 'start_time' col.")
 
 
 def load_data_to_db(json_file_path):
@@ -86,132 +88,97 @@ def load_data_to_db(json_file_path):
     runs_data = pl.DataFrame(data["runs"])
 
     # Connect to db
-    conn = psycopg2.connect(
+    with connect(
         dbname=os.getenv("DBNAME"),
         user=os.getenv("DBUSER"),
         password=os.getenv("DBPASSWD"),
         host=os.getenv("DBHOST"),
         port=os.getenv("DBPORT"),
-    )
-
-    cursor = conn.cursor()
-
-    # PostgreSQL tables
-    create_db_tables = [
-        """
-        CREATE TABLE IF NOT EXISTS runs (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        completion_time INT,
-        affix_names TEXT,
-        level INT,
-        map_name TEXT,
-        start_time TIMESTAMP
-        );
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS party (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        run_id UUID REFERENCES runs(id),
-        role TEXT,
-        name TEXT,
-        class TEXT,
-        spec TEXT 
-        );
-        """,
-    ]
-
-    # Create tables
-    for query in create_db_tables:
-        cursor.execute(query)
-    conn.commit()
-
-    # Ensure constraint
-    ensure_unique_constraint(conn)
-
-    # Insert data into the 'runs' table
-    runs_data = [
-        (
-            run["completionTime"],
-            run["affixNames"],
-            run["level"],
-            run["mapName"],
-            run["startTime"],
-        )
-        for run in data["runs"]
-    ]
-
-    run_ids = []
-    for run in runs_data:
-        cursor.execute(
+    ) as conn:
+        # PostgreSQL tables
+        create_db_tables = [
             """
-            INSERT INTO runs (completion_time, affix_names, level, map_name, start_time)
-            VALUES (%s, %s, %s, %s, %s)
-            ON CONFLICT (start_time) DO UPDATE
-            SET completion_time = EXCLUDED.completion_time,
-                affix_names = EXCLUDED.affix_names,
-                level = EXCLUDED.level,
-                map_name = EXCLUDED.map_name
-            RETURNING id;
+            CREATE TABLE IF NOT EXISTS runs (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                completion_time INT,
+                affix_names TEXT,
+                level INT,
+                map_name TEXT,
+                start_time TIMESTAMP
+            );
             """,
-            run,
-        )
-        run_ids.append(cursor.fetchone()[0])
+            """
+                CREATE TABLE IF NOT EXISTS party (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                run_id UUID REFERENCES runs(id),
+                role TEXT,
+                name TEXT,
+                class TEXT,
+                spec TEXT 
+            );
+            """,
+        ]
 
-    # Prepare party data
-    party_records = []
-    for run_id, run in zip(run_ids, data["runs"]):
-        for party_member in run["party"]:
-            party_records.append(
-                (
-                    run_id,
-                    party_member["role"],
-                    party_member["name"],
-                    party_member["class"],
-                    party_member.get("spec"),
+        with conn.cursor() as cursor:# Create tables
+            for query in create_db_tables:
+                cursor.execute(query)
+            conn.commit()
+
+        # Ensure constraint
+        ensure_unique_constraint(conn)
+
+        # Insert data into the 'runs' table
+        run_ids = []
+        with conn.cursor() as cursor:
+            for run in data["runs"]:
+                try:
+                    cursor.execute(
+                        """
+                        INSERT INTO runs (completion_time, affix_names, level, map_name, start_time)
+                        VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (start_time) DO UPDATE
+                        SET completion_time = EXCLUDED.completion_time,
+                            affix_names = EXCLUDED.affix_names,
+                            level = EXCLUDED.level,
+                            map_name = EXCLUDED.map_name
+                        RETURNING id;
+                        """,
+                        (
+                            run["completionTime"],
+                            run["affixNames"],
+                            run["level"],
+                            run["mapName"],
+                            run["startTime"],
+                        ),
+                    )
+                    run_ids.append(cursor.fetchone()[0])
+                except UniqueViolation:
+                    print(f"Duplicate start_time detected for {run['startTime']}")
+            conn.commit()
+
+        # Insert data into 'party' table
+        party_records = []
+        for run_id, run in zip(run_ids, data["runs"]):
+            for party_member in run["party"]:
+                party_records.append(
+                    (
+                        run_id,
+                        party_member["role"],
+                        party_member["name"],
+                        party_member["class"],
+                        party_member.get("spec"),
+                    )
                 )
+
+        with conn.cursor() as cursor:
+            cursor.executemany(
+                """
+                INSERT INTO party (run_id, role, name, class, spec)
+                VALUES %s
+                ON CONFLICT DO NOTHING;
+                """,
+                party_records,
             )
+            conn.commit()
+        print(f"db {os.getenv("DBNAME")} populated")
 
-    execute_values(
-        cursor,
-        """
-        INSERT INTO party (run_id, role, name, class, spec)
-        VALUES %s
-        ON CONFLICT DO NOTHING;
-        """,
-        party_records,
-    )
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print(f"db {os.getenv("DBNAME")} populated")
-
-
-def ensure_unique_constraint(conn):
-    cursor = conn.cursor()
-
-    cursor.execute(
-        """
-            SELECT conname
-            FROM pg_constraint
-            WHERE conrelid = 'runs'::regclass 
-                AND contype = 'u' 
-                AND conname = 'unique_start_time';
-        """
-    )
-    constraint_exists = cursor.fetchone()
-
-    if not constraint_exists:
-        # Add constraint
-        cursor.execute(
-            """
-                ALTER TABLE runs
-                ADD CONSTRAINT unique_start_time UNIQUE (start_time);
-            """
-        )
-        print("Unique constraint added to 'start_time' col.")
-    else:
-        print("Unique constraint on 'start_time' already exists.")
-
-    conn.commit()
-    cursor.close()
