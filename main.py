86be2d3957cf7397ed6fc229fe79ba50c269a349
Christopher Vestman <christophervestman@gmail.com>
2024-12-05 21:26:18 +0100

    refactor: try/except

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ if __name__ == "__main__":
     try:
         if not db_exists():
             create_db()
-            convert_lua_to_json("MPlusTracker.lua", "MPlusTracker.json")
-            load_data_to_db("MPlusTracker.json")
-        else:
-            print("Updating DB")
-            convert_lua_to_json("MPlusTracker.lua", "MPlusTracker.json")
-            load_data_to_db("MPlusTracker.json")
-
     except Exception as e:
         print(f"Error from main.py: {e}")
+
+    # Updating DB
+    print("Updating DB")
+    convert_lua_to_json("MPlusTracker.lua", "MPlusTracker.json")
+    load_data_to_db("MPlusTracker.json")
